ad572d976ca2e213567ebfd2379cee9fa7e8ad27
Sai <63241537+Hayden-Fade61@users.noreply.github.com>
2024-08-11 05:31:03 +0200

    Started work on grid creation functionality

---
+++ scripts.js
@@ -0,0 +1,27 @@
+/* Create grid */
+const ROW_COUNT = 4;
+const COLUMN_COUNT = 4;
+document.addEventListener("DOMContentLoaded", () => {
+  const sketchContainer = document.querySelector(".canvas");
+  for (let i = 0; i<ROW_COUNT; i++){
+    sketchContainer.appendChild(createRow());
+  }
+})
+
+function createColumn(){
+  const column = document.createElement("div");
+  column.classList.toggle("col");
+  column.addEventListener("hover", () =>{
+    column.style.backgroundColor = "lightpink";
+  })
+  return column;
+}
+
+function createRow(){
+  const row = document.createElement("div");
+  row.classList.toggle("row");
+  for (let j = 0; j<COLUMN_COUNT; j++){
+    row.appendChild(createColumn());
+  }
+  return row;
+}
