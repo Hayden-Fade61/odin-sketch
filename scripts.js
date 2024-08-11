f791d2cd334670fc65126f66d0e24a22699e1dd1
Sai <63241537+Hayden-Fade61@users.noreply.github.com>
2024-08-11 06:38:10 +0200

    Add functionality for user to control grid size by clicking the button

--- scripts.js
+++ scripts.js
@@ -1,12 +1,46 @@
-/* Create grid */
-const ROW_COUNT = 4;
-const COLUMN_COUNT = 4;
-document.addEventListener("DOMContentLoaded", () => {
-  const sketchContainer = document.querySelector(".canvas");
-  for (let i = 0; i<ROW_COUNT; i++){
-    sketchContainer.appendChild(createRow());
+const INITIAL_SIZE = 16;
+const SIZE_LIMIT = 100;
+
+document.addEventListener("DOMContentLoaded", () => {createGrid(INITIAL_SIZE)}) // This event fires when page is done loading
+const  resizeButton = document.querySelector("#resize");
+resizeButton.addEventListener('click', function() {
+  let size = prompt('Please enter grid size as a number:'); // Prompt returns null if you hit cancel
+  if(size > 100){
+    alert("Size is too large, creating largest grid of size 100");
+    size = 100;
   }
-})
+  if (size != null && size > 0){
+    destroyGrid();
+    createGrid(size);
+  }
+});
+
+function createGrid(size){
+  const sketchContainer = document.querySelector(".canvas");
+  for (let i = 0; i<size; i++){
+    sketchContainer.appendChild(createRow(size));
+  }
+}
+
+function destroyGrid(){
+  const cols = document.querySelectorAll(".col");
+  const rows = document.querySelectorAll(".row");
+  cols.forEach((col) => {
+    col.remove();
+  })   
+  rows.forEach((row) => {
+    row.remove();
+  })
+}
+
+function createRow(col_count){
+  const row = document.createElement("div");
+  row.classList.toggle("row");
+  for (let j = 0; j<col_count; j++){
+    row.appendChild(createColumn());
+  }
+  return row;
+}
 
 function createColumn(){
   const column = document.createElement("div");
@@ -15,13 +49,4 @@ function createColumn(){
   column.addEventListener("mouseover", () => {column.classList.toggle("drawn");});
   column.addEventListener("mouseleave", () => {column.classList.toggle("drawn");});
   return column;
-}
-
-function createRow(){
-  const row = document.createElement("div");
-  row.classList.toggle("row");
-  for (let j = 0; j<COLUMN_COUNT; j++){
-    row.appendChild(createColumn());
-  }
-  return row;
 }
